cf7214e8b04ed36c03ab9917d0b3e523ca4c53a1
unknown <klvnafriyie123@gmail.com>
2024-01-10 01:22:24 -0800

    Initial commit

---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import {FaGithub,FaLinkedin} from "react-icons/fa";
+import {HiOutlineMail} from "react-icons/hi";
+import {BsFillPersonLinesFill} from "react-icons/bs";
+
+const SocialLinks = () => {
+    const links = [
+        {
+            id:1,
+            child: (
+                <>
+                    LinkedIn <FaLinkedin size={30}/>
+                </>
+            ),
+            href:"https://linkedin.com",
+            style:"rounded-tr-md"
+        },
+        {
+            id:2,
+            child: (
+                <>
+                    GitHub <FaGithub size={30}/>
+                </>
+            ),
+            href:"https://github.com/blackmoor01",
+        },
+        {
+            id:3,
+            child: (
+                <>
+                    Mail <HiOutlineMail size={30}/>
+                </>
+            ),
+            href:"mailto:klvnafriyie123@gmail.com",
+        },
+        {
+            id:4,
+            child: (
+                <>
+                    Resume <BsFillPersonLinesFill size={30}/>
+                </>
+            ),
+            href:"/KELVIN AFRIYIE RESUME.pdf",
+            style:"rounded-br-md",
+            download:true
+        }
+    ]
+    return(
+        <div className={"hidden lg:flex flex-col top-[35%] left-0 fixed"}>
+            <ul>
+                {links.map(({id, child, href, style, download})=>(
+                    <li key={id} className={"flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] duration-300 hover:rounded-md" + style}><a className={"flex justify-between items-center w-full text-white"} download={download} rel="noreferrer"  href={href} target={"_blank"}>
+                        <>
+                        {child}
+                        </>
+                    </a></li>
+                ))}
+            </ul>
+
+        </div>
+    )
+};
+
+export default SocialLinks;
